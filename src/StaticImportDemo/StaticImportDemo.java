9399384e759aa9175984c07956b1a9e187502fa1
Rima Akter <rimawahid3@gmail.com>
2021-10-07 10:59:24 +0600

    Static import

---
+++ src/StaticImportDemo/StaticImportDemo.java
@@ -0,0 +1,14 @@
+
+package StaticImportDemo;
+
+import static java.lang.Math.PI;
+
+
+public class StaticImportDemo {
+    public static void main(String[] args) {
+        System.out.println(PI);
+        System.out.println(Math.E);
+        System.out.println(Math.sqrt(9));
+        System.out.println( Integer.parseInt("45"));
+    }
+}
